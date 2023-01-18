4a66caac9f6095791858753500e313a58d8a6428
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-18 20:21:34 +0300

    #289476 #382298 ss

--- src/main/java/utils/HabrCareerDateTimeParser.java
+++ src/main/java/utils/HabrCareerDateTimeParser.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
-
+    private static final String pattern = "uuuu-MM-dd'T'HH:mm:ssXXX";
     @Override
     public LocalDateTime parse(String parse) {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ssXXX");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         LocalDateTime parsedDate = LocalDateTime.parse(parse, formatter);
         return parsedDate;
     }

400583206a50bb269d441fdcf2a88fa9663330de
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-18 20:57:46 +0300

    #285213

--- src/main/java/HabrCareerParse.java
+++ src/main/java/HabrCareerParse.java
@@ -3,20 +3,25 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import utils.DateTimeParser;
 import utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 
 public class HabrCareerParse {
 
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
     private static final int MAX_PAGE = 5;
 
     public static void main(String[] args) throws IOException {
-        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
-        HabrCareerParse careerHabr = new HabrCareerParse();
+        HabrCareerParse careerHabr = new HabrCareerParse(new HabrCareerDateTimeParser());
         for (int pageIterator = 1; pageIterator < MAX_PAGE; pageIterator = pageIterator + 1) {
             Connection connection = Jsoup.connect(PAGE_LINK + pageIterator);
             Document document = connection.get();
@@ -27,7 +32,7 @@ public class HabrCareerParse {
                 Element linkElement = titleElement.child(0);
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s  %n", vacancyName, link, parser.parse(date));
+                System.out.printf("%s %s %s  %n", vacancyName, link, careerHabr.dateTimeParser.parse(date));
 
                 try {
                     System.out.printf(careerHabr.retrieveDescription(link));
---
+++ src/main/java/Parse.java
@@ -0,0 +1,7 @@
+import Model.Post;
+
+import java.util.List;
+
+public interface Parse {
+    List<Post> list(String link);
+}

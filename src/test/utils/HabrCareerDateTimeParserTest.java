aca9cb913804f812fac17e2b4e5397347a2e7b04
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-18 20:18:39 +0300

    #289476 #382298

---
+++ src/test/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,15 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+     void checkDate(){
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        assertThat(parser.parse("2011-12-03T10:15:30+01:00")).isEqualTo("2011-12-03T10:15:30");
+    }
+
+}

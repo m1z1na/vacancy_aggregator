8caab55b99c2a95200120bdb0c3df2d917401d35
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-20 17:54:37 +0300

    #289476 #382298

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -2,7 +2,7 @@ import Model.Post;
 
 import java.util.List;
 
-public interface Store {
+public interface Store  extends AutoCloseable {
     void save(Post post);
 
     List<Post> getAll();
--- src/main/java/utils/HabrCareerDateTimeParser.java
+++ src/main/java/utils/HabrCareerDateTimeParser.java
@@ -5,10 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
     private static final String pattern = "uuuu-MM-dd'T'HH:mm:ssXXX";
+    private static final DateTimeFormatter formatter  = DateTimeFormatter.ofPattern(pattern);
+
+
+
     @Override
     public LocalDateTime parse(String parse) {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         LocalDateTime parsedDate = LocalDateTime.parse(parse, formatter);
         return parsedDate;
     }

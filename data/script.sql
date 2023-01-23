27e4a2aeabf196d2f6b2124996d8c55559d13a1d
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-23 19:18:39 +0300

    #1731 #382592

--- data/script.sql
+++ data/script.sql
@@ -1,7 +1,7 @@
 create table post(
- id serial primary key,
-  name text,
-   text text,
-    link text,
+    id serial primary key,
+    name text,
+    text text,
+    link text UNIQUE,
      created timestamp
 );
